5826e8a732400865269a3527a94907900fa5dc52
Facundo Soria <facundo.soria@devecoop.com>
2015-11-03 10:24:05 -0300

    Output format

--- script.py
+++ script.py
@@ -32,14 +32,11 @@ cards = trello.lists.get(list_id, cards="open", card_fields="name,desc,shortUrl,
 for card in cards:
     print "== " + card.get("name") + " =="
     print "Asignados: " + ", ".join([members[m] for m in card.get("idMembers")])
-    print "\n"
     print card.get("desc")
     comments = trello.cards.get_action(card.get("id"), filter="commentCard")
     if comments:
-        print "\n"
         print "Comments:"
     for comment in comments:
-        print "\n"
         print comment.get("memberCreator").get("fullName") + "(" + comment.get("date") + ") :"
         print comment.get("data").get("text")
     print "\n"
